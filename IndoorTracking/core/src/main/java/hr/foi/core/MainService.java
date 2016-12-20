28426d7e1ce7afc77166d1ee91f99d9149e9b9f1
Paula <kokic.paula@zd.t-com.hr>
2016-12-20 16:00:56 +0100

    Push notifikacije, osnovno :D

--- IndoorTracking/core/src/main/java/hr/foi/core/MainService.java
+++ IndoorTracking/core/src/main/java/hr/foi/core/MainService.java
@@ -1,14 +1,19 @@
 package hr.foi.core;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.app.TaskStackBuilder;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
 import android.os.Message;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -126,7 +131,41 @@ public class MainService extends Service {
 //                }
                 if (nearestSensor != null && lastSensor != nearestSensor){
                     String snrName = nearestSensor.getSnrBleMac(); //TODO: DOHVAĆANJE LOKACIJE PREMA MAC ADRESI
-                    Toast.makeText(this, snrName + " frame: "+nearestSensor.getSnrSignalZ(), Toast.LENGTH_SHORT).show();
+
+                    NotificationCompat.Builder mBuilder =
+                            new NotificationCompat.Builder(this)
+                                    .setSmallIcon(R.drawable.notification_icon)
+                                    .setContentTitle("Indoor Tracking")
+                                    .setContentText("Welcome to "+ snrName+ "!");
+// Creates an explicit intent for an Activity in your app
+
+                    //Intent resultIntent = new Intent(this, ResultActivity.class);
+
+// The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+                    TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+// Adds the back stack for the Intent (but not the Intent itself)
+                    //stackBuilder.addParentStack(ResultActivity.class);
+// Adds the Intent that starts the Activity to the top of the stack
+
+                    /*stackBuilder.addNextIntent(resultIntent);
+                    PendingIntent resultPendingIntent =
+                            stackBuilder.getPendingIntent(
+                                    0,
+                                    PendingIntent.FLAG_UPDATE_CURRENT
+                            );
+                    mBuilder.setContentIntent(resultPendingIntent); */
+
+                    NotificationManager mNotificationManager =
+                            (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+
+                    mNotificationManager.notify(nearestSensor.getSnrSignalZ(), mBuilder.build());
+
+
+                    //Toast.makeText(this, snrName + " frame: "+nearestSensor.getSnrSignalZ(), Toast.LENGTH_SHORT).show();
                     //txtCurrentLocation.setText(snrName + " frame: "+nearestSensor.getSnrSignalZ());
                     //txtCurrentLocation.setText(snrName+ " " +nearestSensor.getSnrSignalZ());
 
