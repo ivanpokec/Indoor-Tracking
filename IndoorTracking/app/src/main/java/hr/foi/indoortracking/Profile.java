faf0f721794f8a5dab36707e4f8b030ae8d64409
zanzekic <zanzekic@foi.hr>
2016-12-20 09:04:09 +0100

    Uređenje klase profil

--- IndoorTracking/app/src/main/java/hr/foi/indoortracking/Profile.java
+++ IndoorTracking/app/src/main/java/hr/foi/indoortracking/Profile.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Zana on 19.12.2016..
@@ -12,9 +13,14 @@ import android.widget.Button;
 
 public class Profile  extends AppCompatActivity {
 
+    private TextView nameTextView;
+    private TextView surnameTextView;
+    private TextView usernameTextView;
+    private TextView passwordTextView;
+    private TextView odjelTextView;
     Button button_LogOut;
-
     SessionManager manager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,14 @@ public class Profile  extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         button_LogOut = (Button) findViewById(R.id.button_LogOut);
         manager = new SessionManager();
+        nameTextView = (TextView) findViewById(R.id.textview_name);
+        surnameTextView = (TextView) findViewById(R.id.textview_surname);
+        usernameTextView = (TextView) findViewById(R.id.textview_username);
+        passwordTextView = (TextView) findViewById(R.id.textview_password);
+        odjelTextView = (TextView) findViewById(R.id.textview_odjel);
+
+       // usernameTextView.setText(String.format("Korisničko ime: %s",user.getUsername()));
+
      button_LogOut.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
